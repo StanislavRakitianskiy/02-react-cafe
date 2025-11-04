e4a26f551886a3fbea61d8e31e0fa691b882596e
StanislavRakitianskiy <raketaswig@gmail.com>
2025-11-04 13:03:57 +0200

    add files

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,5 +1,9 @@
 import { useState } from "react";
 import CafeInfo from "../CafeInfo/CafeInfo";
+import VoteOptions from "../VoteOptions/VoteOptions";
+import VoteStats from "../VoteStats/VoteStats";
+import Notification from "../Notification/Notification";
+
 import type { Votes, VoteType } from "../../types/votes";
 import css from "./App.module.css";
 import "../../css/App.css";
@@ -18,9 +22,19 @@ function App() {
     }));
   };
   const resetVotes = () => setVotes(initialVotes);
+  const totalVotes = votes.good + votes.neutral + votes.bad;
+  const positiveRate = totalVotes
+    ? Math.round((votes.good / totalVotes) * 100)
+    : 0;
   return (
     <div className={css.app}>
       <CafeInfo></CafeInfo>
+      <VoteOptions
+        onVote={handleVote}
+        onReset={resetVotes}
+        canReset={totalVotes > 0}
+      ></VoteOptions>
+      {totalVotes > 0 ? (<VoteStats votes={votes} totalVotes={totalVotes} positiveRate={positiveRate}></VoteStats>) : (<Notification></Notification>) } 
     </div>
   );
 }
